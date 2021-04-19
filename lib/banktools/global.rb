e3873dfe694c8da7f3fc27f7ab0e6e71de120896
Henrik Nyh <henrik@nyh.se>
2021-04-19 08:39:49 +0100

    Generated gem

---
+++ lib/banktools/global.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "global/version"
+
+module Banktools
+  module Global
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
